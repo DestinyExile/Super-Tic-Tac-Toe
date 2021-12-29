55d6ad54800aff1e4df762af2390d520eee511f3
Marcus Chang <marxalex7@gmail.com>
2021-12-29 15:12:41 -0600

    Added UI to indicate current player's turnn

--- Assets/TurnController.cs
+++ Assets/TurnController.cs
@@ -4,11 +4,28 @@ using UnityEngine;
 
 public class TurnController : MonoBehaviour
 {
+    [SerializeField] GameObject player1TurnText = null;
+    [SerializeField] GameObject player2TurnText = null;
+
     public static int turnCount = 1;
+
+    private void Update()
+    {
+        if(turnCount % 2 == 0)
+        {
+            player2TurnText.SetActive(true);
+            player1TurnText.SetActive(false);
+        }
+        else
+        {
+            player1TurnText.SetActive(true);
+            player2TurnText.SetActive(false);
+        }
+    }
 
     public static void changeTurn()
     {
         turnCount++;
-        Debug.Log(turnCount);
+
     }
 }
