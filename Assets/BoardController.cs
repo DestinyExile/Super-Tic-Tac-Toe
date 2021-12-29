5b7c6a22d4f321a6cafa902d8f6bc2d1fc91c1d1
Marcus Chang <marxalex7@gmail.com>
2021-12-29 13:41:24 -0600

    Added win functionality for sub board and reset function

---
+++ Assets/BoardController.cs
@@ -0,0 +1,130 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardController : MonoBehaviour
+{
+    [SerializeField] SlotController Slot1 = null;
+    [SerializeField] SlotController Slot2 = null;
+    [SerializeField] SlotController Slot3 = null;
+    [SerializeField] SlotController Slot4 = null;
+    [SerializeField] SlotController Slot5 = null;
+    [SerializeField] SlotController Slot6 = null;
+    [SerializeField] SlotController Slot7 = null;
+    [SerializeField] SlotController Slot8 = null;
+    [SerializeField] SlotController Slot9 = null;
+    [SerializeField] GameObject xMarker = null;
+    [SerializeField] GameObject oMarker = null;
+
+    private void Update()
+    {
+        checkWin();
+    }
+
+    private void checkWin()
+    {
+        //horizontal first row
+        if (Slot1.slotState == Slot2.slotState && Slot2.slotState == Slot3.slotState)
+        {
+            if (Slot1.slotState == 1)
+            {
+                xMarker.SetActive(true);
+            }
+            if (Slot1.slotState == 2)
+            {
+                oMarker.SetActive(true);
+            }
+        }
+
+        //horizontal second row
+        if (Slot4.slotState == Slot5.slotState && Slot5.slotState == Slot6.slotState)
+        {
+            if (Slot4.slotState == 1)
+            {
+                xMarker.SetActive(true);
+            }
+            if (Slot4.slotState == 2)
+            {
+                oMarker.SetActive(true);
+            }
+        }
+
+        //horizontal third row
+        if (Slot7.slotState == Slot8.slotState && Slot8.slotState == Slot9.slotState)
+        {
+            if (Slot7.slotState == 1)
+            {
+                xMarker.SetActive(true);
+            }
+            if (Slot7.slotState == 2)
+            {
+                oMarker.SetActive(true);
+            }
+        }
+
+        //veritcal first columnn
+        if (Slot1.slotState == Slot4.slotState && Slot4.slotState == Slot7.slotState)
+        {
+            if (Slot1.slotState == 1)
+            {
+                xMarker.SetActive(true);
+            }
+            if (Slot1.slotState == 2)
+            {
+                oMarker.SetActive(true);
+            }
+        }
+
+        //veritcal second columnn
+        if (Slot2.slotState == Slot5.slotState && Slot5.slotState == Slot8.slotState)
+        {
+            if (Slot2.slotState == 1)
+            {
+                xMarker.SetActive(true);
+            }
+            if (Slot2.slotState == 2)
+            {
+                oMarker.SetActive(true);
+            }
+        }
+
+        //veritcal third columnn
+        if (Slot3.slotState == Slot6.slotState && Slot6.slotState == Slot9.slotState)
+        {
+            if (Slot3.slotState == 1)
+            {
+                xMarker.SetActive(true);
+            }
+            if (Slot3.slotState == 2)
+            {
+                oMarker.SetActive(true);
+            }
+        }
+
+        //right diagonal
+        if (Slot1.slotState == Slot5.slotState && Slot5.slotState == Slot9.slotState)
+        {
+            if (Slot1.slotState == 1)
+            {
+                xMarker.SetActive(true);
+            }
+            if (Slot1.slotState == 2)
+            {
+                oMarker.SetActive(true);
+            }
+        }
+
+        //left diagonal
+        if (Slot3.slotState == Slot5.slotState && Slot5.slotState == Slot7.slotState)
+        {
+            if (Slot3.slotState == 1)
+            {
+                xMarker.SetActive(true);
+            }
+            if (Slot3.slotState == 2)
+            {
+                oMarker.SetActive(true);
+            }
+        }
+    }
+}
--- Assets/SlotController.cs
+++ Assets/SlotController.cs
@@ -8,6 +8,8 @@ public class SlotController : MonoBehaviour
     [SerializeField] GameObject xMarker = null;
     [SerializeField] GameObject oMarker = null;
 
+    public int slotState = 0;
+
     private Image slotImage;
 
     private void Start()
@@ -19,13 +21,19 @@ public class SlotController : MonoBehaviour
     {
         if(TurnController.turnCount % 2 == 1)
         {
-            if(xMarker != null)
+            if (xMarker != null)
+            {
                 xMarker.SetActive(true);
+                slotState = 1;
+            }
         }
         if(TurnController.turnCount % 2 == 0)
         {
             if (oMarker != null)
+            {
                 oMarker.SetActive(true);
+                slotState = 2;
+            }
         }
 
         slotImage.raycastTarget = false;
